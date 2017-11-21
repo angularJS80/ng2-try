f8207a0417502c428c00332decde49a5aefbaa91
angularjs80 <bigcen@naver.com>
2017-11-21 21:11:53 +0900

    미정의된 변수사용 html 주석처리 및 미사용 서비스 주석처리

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 ﻿import { Component, OnInit } from '@angular/core';
 
 import { User } from '../_models/index';
-import { UserService } from '../_services/index';
+//import { UserService } from '../_services/index';
 
 @Component({
     moduleId: module.id,
@@ -12,19 +12,21 @@ export class HomeComponent implements OnInit {
     currentUser: User;
     users: User[] = [];
 
-    constructor(private userService: UserService) {
+    constructor(
+      //private userService: UserService
+    ) {
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
     }
 
     ngOnInit() {
-        this.loadAllUsers();
+       // this.loadAllUsers();
     }
 
-    deleteUser(id: number) {
+   /* deleteUser(id: number) {
         this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
     }
 
     private loadAllUsers() {
         this.userService.getAll().subscribe(users => { this.users = users; });
-    }
+    }*/
 }
