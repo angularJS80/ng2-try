e792b3c43d424f2f4088b3c30eee7614a0e2e3e7
angularjs80 <bigcen@naver.com>
2018-01-23 14:12:02 +0900

    file업로드 추가

--- src/app/attach/attach.component.ts
+++ src/app/attach/attach.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FileUploader} from "ng2-file-upload";
+import {FileUploader, FileItem} from "ng2-file-upload";
+import {ApiRequestService} from "../_services/apiRequest.service";
 
 @Component({
   selector: 'app-attach',
@@ -7,20 +8,51 @@ import {FileUploader} from "ng2-file-upload";
   styleUrls: ['./attach.component.css']
 })
 export class AttachComponent implements OnInit {
+  apiurl:string = "http://localhost:38080/api/fileupload";
   uploader:FileUploader = new FileUploader({
-    url:"http://localhost:38080/api/fileupload"
+    url:this.apiurl
   });
-  constructor() {
+  completefilelist=[];
+  cancelfilelist=[];
+
+  constructor(public apirequestService:ApiRequestService) {
     this.uploader.onAfterAddingFile = (item => {
       item.withCredentials = false;
+
+      // 서버에 전송해야 함으로 오버라이드 개념으로 재정의 하고 기존에 호출하던 cancelItem 호출
+      item.remove = () => { // 제거 시 서버단 삭제 여부 결정필요
+        this.apirequestService.getRequestOptions("delete",this.apiurl+"/"+item.file["file_id"]);
+        this.uploader.removeFromQueue(item);
+        console.log(this.uploader.queue);
+      };
+
+      // 서버에 전송해야 함으로 오버라이드 개념으로 재정의 하고 기존에 호출하던 cancelItem 호출
+      item.cancel = () => { // 업로드 도중 취소시 서버단 삭제 여부 결정필요
+        this.apirequestService.getRequestOptions("delete",this.apiurl+"/"+item.file["file_id"]);
+        this.uploader.cancelItem(item);
+        console.log(this.uploader.queue);
+      };
     });
 
     this.uploader.onCompleteItem = (item, response, status, headers)=>{
+      item.file["file_id"] = JSON.parse(response)._id; // 완료된 파일id를 서버로 부터 리턴 받는다.
+      this.completefilelist.push(response);
       console.log(response);
     }
-  }
+
+    this.uploader.onCancelItem = (item, response, status, headers)=>{
+      console.log("onCancelItem ");
+      this.cancelfilelist.push(response);
+      console.log(response);
+    }
+
+    this.uploader.onCompleteAll = () => { // 모든 파일이 업로드완료시 아래 queue 에 file_id 를 컨텐츠에 컨텐츠에 종속 시킨다.
+      console.log('complete');
+      console.log(this.uploader.queue);
+    };
+  };
 
   ngOnInit() {
-  }
 
+  }
 }
