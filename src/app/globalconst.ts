50c8552d9e61b9c08a2056661fc6a70424add8ae
angularJS80 <bigcen@naver.com>
2018-10-08 08:05:25 +0900

    오류 수정

--- src/app/globalconst.ts
+++ src/app/globalconst.ts
@@ -2,13 +2,13 @@
  * Created by jcompia on 2018-01-25.
  */
 export class GlobalConst {
+
+  public static get HEROAPI_ENDPOINT(): string { return 'http://52.194.130.175:58080/hero'; }
+  public static get NODEAPI_ENDPOINT(): string { return 'http://52.194.130.175:38080/api'; }
+  public static get NODE_ENDPOINT(): string { return 'http://52.194.130.175:38080'; }
   /*
-  public static get HEROAPI_ENDPOINT(): string { return 'http://211.249.60.229:58080/hero'; }
-  public static get NODEAPI_ENDPOINT(): string { return 'http://211.249.60.229:38080/api'; }
-  public static get NODE_ENDPOINT(): string { return 'http://211.249.60.229:38080'; }
-   */
-  public static get HEROAPI_ENDPOINT(): string { return 'http://211.249.60.229:58080/hero'; }
+  public static get HEROAPI_ENDPOINT(): string { return 'http://52.194.130.175:58080/hero'; }
   public static get NODEAPI_ENDPOINT(): string { return 'http://localhost:38080/api'; }
   public static get NODE_ENDPOINT(): string { return 'http://localhost:38080'; }
-
+   */
 }
